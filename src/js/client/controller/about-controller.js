c07d726ed97927b2c43ef4c2cbb8d128aef6c7f4
p1100i <p1100i@gmx.com>
2017-05-19 20:09:29 +0200

    js: cleanup

--- src/js/client/controller/about-controller.js
+++ src/js/client/controller/about-controller.js
@@ -2,7 +2,6 @@ define(['app'], function (app) {
   app.controller('aboutController', [function AboutControllerFactory() {
     var
       init = function init() {
-        console.log('aboutController - init, YAY \\o_');
       };
 
     init();
--- src/js/client/controller/statistic-controller.js
+++ src/js/client/controller/statistic-controller.js
@@ -1,8 +1,6 @@
 define(['app'], function (app) {
   app.controller('statisticController', ['settingService', function statisticControllerFactory(settingService) {
     var
-      ctrl = this,
-
       init = function init() {
       };
 
