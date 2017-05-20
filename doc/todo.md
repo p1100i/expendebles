15a00bf99e123bcb5fb5e3ff410ccbf4d158a56c
p1100i <p1100i@gmx.com>
2017-05-20 17:08:19 +0200

    main: adds basic data page for export/import

--- doc/todo.md
+++ doc/todo.md
@@ -1,7 +1,8 @@
 - naiv data page
   + new blank page
-  - display JSON.stringify of localStorage data
-  - allow to fill up localStorage from text-area
+  + display JSON.stringify of localStorage data
+  + allow to fill up localStorage from text-area
+  - add copy-to-clipboard button
 
 + naiv register an expense
   + add plus/minus icon
--- src/js/client/controller/data-controller.js
+++ src/js/client/controller/data-controller.js
@@ -1,9 +1,36 @@
 define(['app'], function (app) {
-  app.controller('dataController', [function DataControllerFactory() {
+  app.controller('dataController', ['storageService', function DataControllerFactory(storageService) {
     var
       ctrl = this,
 
+      importJSON = function importJSON(data) {
+        var
+          currentVersion = storageService.get('version');
+
+        data = JSON.parse(data);
+
+        if (data.version !== currentVersion) {
+          console.error('Version mismatch imported', data.version, 'current', currentVersion);
+          throw new Error('invalid_version');
+        }
+
+        storageService.set('finance', data.finance);
+      },
+
+      exportJSON = function exportJSON() {
+        var
+          result = JSON.stringify({
+            'version' : storageService.get('version'),
+            'finance' : storageService.get('finance')
+          }, 0, 2);
+
+        return result;
+      },
+
       init = function init() {
+        ctrl.importJSON = importJSON;
+
+        ctrl.exportedJSON = exportJSON();
       };
 
     init();
