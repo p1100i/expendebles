56757710e9e20864fc4571f657ec95be00951a6a
p1100i <p1100i@gmx.com>
2017-05-21 18:16:24 +0200

    header: disable statistics until it has "real" feature

--- src/js/client/controller/app-controller.js
+++ src/js/client/controller/app-controller.js
@@ -18,7 +18,7 @@ define(['app'], function (app) {
 
       setMenu = function setMenu() {
         anchors.push(createAnchor('/reg',         '#!/register',  'Register spendings'));
-        anchors.push(createAnchor('/stat',        '#!/statistic', 'View statistics'));
+        // anchors.push(createAnchor('/stat',        '#!/statistic', 'View statistics'));
         anchors.push(createAnchor('/data',        '#!/data',      'Export/import data'));
         anchors.push(createAnchor('expendebles',  '#!/',          'About', 'bank'));
       },
