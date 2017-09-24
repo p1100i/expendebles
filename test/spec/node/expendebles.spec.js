ac354fa89300fee11e16797ffaedaa13cde30aee
p1100i <p1100i@gmx.com>
2017-09-24 14:18:05 +0200

    grunt: disables unnecessary test execution

--- test/spec/node/expendebles.spec.js
+++
@@ -1,9 +0,0 @@
-var
-  requireHelper = require('../../require-helper'),
-  fn            = requireHelper('./expendebles');
-
-describe('fn', function () {
-  it('should return 1', function () {
-    expect(fn()).toEqual(1);
-  });
-});
