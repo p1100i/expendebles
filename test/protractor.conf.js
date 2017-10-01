d43560d9c88d4c500752f582a7d4a78ebc0ea5d4
p1100i <p1100i@gmx.com>
2017-10-01 17:06:18 +0200

    test: adds basic integration testing
    * needs rework + specs

---
+++ test/protractor.conf.js
@@ -0,0 +1,6 @@
+exports.config = {
+  'directConnect' : true,
+  'allScriptsTimeout' : 3000,
+  'baseUrl'           : 'http://localhost:3000',
+  'specs'             : ['./spec/integration/*.spec.js']
+};
