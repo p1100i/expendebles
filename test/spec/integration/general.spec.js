d4076de85252087fd38c7758a62313dd1c50052b
p1100i <p1100i@gmx.com>
2018-01-01 13:41:26 +0100

    test: redesign general integration spec
    * use separate navigation functions
    * refactor common logic to helper functions
    * split scenario to more exact test steps

--- test/spec/integration/general.spec.js
+++ test/spec/integration/general.spec.js
@@ -1,57 +1,121 @@
 var
+  setLastUsedInterval = function setLastUsedInterval() {
+    browser.executeScript('localStorage.setItem(\'app.interval\', 1506808800000);');
+  },
+
+  navigateToLanding = function navigateToLanding() {
+    browser.get('/');
+  },
+
+  navigateToRegister = function navigateToRegister() {
+    return browser.get('/#!/register');
+  },
+
+  navigateToConfig = function navigateToConfig() {
+    return browser.get('/#!/config');
+  },
+
   testSettingMonthBeginOption = function testSettingMonthBeginOption() {
     var
       monthBeg        = element(by.model('config.monthBeg')),
       monthBegOption  = monthBeg.element(by.cssContainingText('option', '15'));
 
     monthBegOption.click();
+  },
 
-    return browser.get('/#!/register');
+  setAmount = function setAmount(value) {
+    var
+      amount = element(by.id('amount'));
+
+    amount.sendKeys(value);
+  },
+
+  setDate = function setDate(year, month, day, hour, minute) {
+    var
+      input = element(by.model('register.selectedItem.date'));
+
+    //
+    // In my case US locale was set in the chrome process,
+    // however it may resurface as an issue, that during
+    // e2e test execution the users browser has a different
+    // locale setting.
+    //
+    input.sendKeys([day, month, year].join('/') + protractor.Key.TAB + hour + ':' + minute);
+  },
+
+  stepPrevMonth = function stepPrevMonth() {
+    var
+      left= element(by.css('[title="Previous month"]'));
+
+    left.click();
+  },
+
+  stepNextMonth = function stepNextMonth() {
+    var
+      right = element(by.css('[title="Next month"]'));
+
+    right.click();
+  },
+
+
+  startEdit = function startEdit() {
+    var
+      edit = element(by.css('.edit'));
+
+    edit.click();
+  },
+
+  finishEdit = function finishEdit() {
+    var
+      done = element(by.css('[title="Done"]'));
+
+    done.click();
+  },
+
+  getSelectedText = function getSelectedText() {
+    return element(by.css('tr.selected td.amount-cell')).getText();
+  },
+
+  getIntervalText = function getIntervalText() {
+    return element(by.binding('app.interval.title')).getText();
+  },
+
+  expectRegisterHasItem = function expectRegisterHasItem(amount) {
+    var
+      item = element(by.css('table.items tr td.amount-cell'));
+
+    expect(item.getText()).toBe(amount);
+  },
+
+  expectRegisterHasItems = function expectRegisterHasItems(expectedValue) {
+    var
+      items = element(by.css('table.items'));
+
+    expect(items.isPresent()).toBe(expectedValue);
   },
 
   testRegisterAddAndEdit= function testRegisterAddAndEdit() {
-    var
-      done,
-      input,
-      selected,
-      showedInterval,
-      amount  = element(by.id('amount')),
-      edit    = element(by.css('.edit')),
-      left    = element(by.css('[title="Previous month"]'));
+    stepPrevMonth();
+    setAmount('111');
+    startEdit();
 
-    left.click();
+    expect(getIntervalText()).toBe('\'17 Sep');
 
-    amount.sendKeys('111');
+    setDate('2017', '10', '09', '08', '40');
 
-    edit.click();
+    expect(getSelectedText()).toBe('111');
+    expect(getIntervalText()).toBe('\'17 Sep');
 
-    input           = element(by.model('register.selectedItem.date'));
-    showedInterval  = element(by.binding('app.interval.title'));
+    finishEdit();
 
-    expect(showedInterval.getText()).toBe('\'17 Sep');
+    expectRegisterHasItems(false);
 
-    input.sendKeys('09/10/2017' + protractor.Key.TAB + '08:40');
+    stepPrevMonth();
 
-    selected = element(by.css('tr.selected td.amount-cell'));
+    expect(getIntervalText()).toBe('\'17 Aug');
 
-    expect(selected.getText()).toBe('111');
-
-    expect(showedInterval.getText()).toBe('\'17 Sep');
-
-    done = element(by.css('[title="Done"]'));
-
-    done.click();
-
-    expect(element(by.css('table.items')).isPresent()).toBe(false);
-
-    left.click();
-
-    expect(showedInterval.getText()).toBe('\'17 Aug');
-
-    expect(element(by.css('table.items')).isPresent()).toBe(true);
-    expect(element(by.css('table.items tr td.amount-cell')).getText()).toBe('111');
-
-    return browser.get('/#!/config');
+    expectRegisterHasItems(true);
+    expectRegisterHasItem('111');
   },
 
   testSerializationWithReconfiguration = function testSerializationWithReconfiguration() {
@@ -83,41 +147,46 @@ var
     importButton.click();
 
     expect(monthBeg.getAttribute('value')).toEqual('number:7');
-
-    return browser.get('/#!/register');
   },
 
   testRegisteredItemUpdate = function testRegisteredItemUpdate() {
-    var
-      showedInterval  = element(by.binding('app.interval.title')),
-      right           = element(by.css('[title="Next month"]'));
+    expect(getIntervalText()).toBe('\'17 Aug');
 
+    expectRegisterHasItems(false);
 
-    expect(showedInterval.getText()).toBe('\'17 Aug');
+    stepNextMonth();
 
-    expect(element(by.css('table.items')).isPresent()).toBe(false);
-
-    right.click();
-
-    expect(element(by.css('table.items tr td.amount-cell')).getText()).toBe('111');
+    expectRegisterHasItems(true);
+    expectRegisterHasItem('111');
   };
 
-describe('page', function () {
-  it('should have the title', function () {
-    browser.get('/');
-
-    expect(browser.getTitle()).toEqual('expendebles');
-
-    browser.executeScript('localStorage.setItem(\'app.interval\', 1506808800000);');
+describe('general', function () {
+  beforeEach(function () {
+    navigateToLanding();
+    setLastUsedInterval();
   });
-});
 
-describe('register', function () {
-  it('should keep data for serialization up-to-date and display updated data on config change', function () {
-    browser.get('/#!/config')
-      .then(testSettingMonthBeginOption)
-      .then(testRegisterAddAndEdit)
-      .then(testSerializationWithReconfiguration)
-      .then(testRegisteredItemUpdate)
+  describe('landing page', function () {
+    it('should have the title', function () {
+      expect(browser.getTitle()).toEqual('expendebles');
+    });
+  });
+
+  describe('register', function () {
+    describe('with custom month begin options', function () {
+      beforeEach(function () {
+        navigateToConfig()
+          .then(testSettingMonthBeginOption);
+      });
+
+      it('should keep data for serialization up-to-date and display updated data on config change', function () {
+        navigateToRegister()
+          .then(testRegisterAddAndEdit)
+          .then(navigateToConfig)
+          .then(testSerializationWithReconfiguration)
+          .then(navigateToRegister)
+          .then(testRegisteredItemUpdate);
+      });
+    });
   });
 });
