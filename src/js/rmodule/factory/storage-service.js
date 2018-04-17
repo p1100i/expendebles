7afd8ce81af68f7fd7f928de218207825f0d0f9c
p1100i <p1100i@gmx.com>
2018-04-17 07:08:16 +0200

    storage: adds upgrade logic for version 5
    * adds .y property w/ 2017 value for persisted existing balance
      entries

--- src/js/rmodule/factory/storage-service.js
+++ src/js/rmodule/factory/storage-service.js
@@ -1,14 +1,14 @@
 define(['app'], function (app) {
   app.factory('storageService', ['$rootScope', '$window', 'localStorageService', function storageServiceFactory($rootScope, $window, localStorageService) {
     var
+      CURRENT_VERSION = 5,
+
       DEFAULTS = {
         'balances'      : [],
         'transactions'  : [],
         'monthBeg'      : 1,
-        'version'       : 4
+        'version'       : CURRENT_VERSION
       },
-
-      CURRENT_VERSION = 4,
 
       MAX_STORAGE_IN_BYTES = 1024 * 1024 * 5,
 
@@ -63,6 +63,14 @@ define(['app'], function (app) {
         }
       },
 
+      /**
+       * Finance was storing items which was not correct
+       * w/ the entry of balances, as they are items as
+       * well.
+       *
+       * This function moves existing .items to
+       * .transactions and adds .balances as well.
+       */
       upgradeTo2 = function upgradeTo2() {
         var
           finance   = getValue('finance'),
@@ -78,6 +86,15 @@ define(['app'], function (app) {
         setValue('finance', finance);
       },
 
+      /**
+       * To make monthly interval configurable there was
+       * no point to keep timestamps in balance items.
+       *
+       * This function removes timestamps and adds .m as
+       * property to track the specific month of the balance.
+       *
+       * (intervals are considered now month-based)
+       */
       upgradeTo3 = function upgradeTo3() {
         var
           month,
@@ -101,6 +118,12 @@ define(['app'], function (app) {
         setValue('finance', finance);
       },
 
+      /**
+       * Finance items were kept in one array as finance, which
+       * was hard to manipulate.
+       *
+       * This function splits them up.
+       */
       upgradeTo4 = function upgradeTo4() {
         var
           finance       = getValue('finance'),
@@ -111,6 +134,28 @@ define(['app'], function (app) {
         setValue('transactions',  transactions);
 
         localStorageService.remove('finance');
+      },
+
+      /**
+       * Balances did not track their year, which caused bugs
+       * in balance registration and in the calculation of
+       * unregistered sums between months/intervals.
+       *
+       * This function add the .y property for balances w/o
+       * any special logic, simply as 2017.
+       */
+      upgradeTo5 = function upgradeTo5() {
+        var
+          balance,
+          balances = getValue('balances'),
+          len = balances.length;
+
+        while (len--) {
+          balance   = balances[len];
+          balance.y = 2017;
+        }
+
+        setValue('balances', balances);
       },
 
       clear = function clear() {
@@ -150,6 +195,7 @@ define(['app'], function (app) {
         upgrades['2'] = upgradeTo2;
         upgrades['3'] = upgradeTo3;
         upgrades['4'] = upgradeTo4;
+        upgrades['5'] = upgradeTo5;
 
         upgrade();
       };
